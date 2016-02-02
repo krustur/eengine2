761ba3759aac739f4f8912faccf36686699280b4
Krister Jansson <krustur@gmail.com>
2016-02-02 23:28:16 +0100

    Moving EEngine code into EEngine namespace

--- EEngine/ETimer.cpp
+++ EEngine/ETimer.cpp
@@ -1,94 +1,97 @@
 #include <windows.h>
 #include "ETimer.h"
 
-ETimer::ETimer() :
-	_secondsPerCounter(0.0),
-	_deltaTime(-1.0),
-	_initTime(0),
-	_pausedTimeSpan(0), 
-	_previousTime(0),
-	_currentTime(0),
-	_isPaused(false),
-	_pauseTime(0)
+namespace EEngine
 {
-	__int64 countsPerSec;
-	QueryPerformanceFrequency((LARGE_INTEGER*)&countsPerSec);
-	_secondsPerCounter = 1.0 / (double)countsPerSec;
-}
-
-float ETimer::GetTotalTime()const
-{
-	if (_isPaused)
+	ETimer::ETimer() :
+		_secondsPerCounter(0.0),
+		_deltaTime(-1.0),
+		_initTime(0),
+		_pausedTimeSpan(0),
+		_previousTime(0),
+		_currentTime(0),
+		_isPaused(false),
+		_pauseTime(0)
 	{
-		return (float)(((_pauseTime - _pausedTimeSpan) - _initTime)*_secondsPerCounter);
+		__int64 countsPerSec;
+		QueryPerformanceFrequency((LARGE_INTEGER*)&countsPerSec);
+		_secondsPerCounter = 1.0 / (double)countsPerSec;
 	}
 
-	return (float)(((_currentTime - _pausedTimeSpan) - _initTime)*_secondsPerCounter);
-}
-
-float ETimer::GetDeltaTime()const
-{
-	return (float)_deltaTime;
-}
-
-
-void ETimer::Init()
-{
-	__int64 currentTime;
-	QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
-
-	_initTime = currentTime;
-	_previousTime = currentTime;
-	_pauseTime = 0;
-	_isPaused = false;
-}
-
-
-void ETimer::Tick()
-{
-	if (_isPaused)
+	float ETimer::GetTotalTime()const
 	{
-		_deltaTime = 0.0;
-		return;
+		if (_isPaused)
+		{
+			return (float)(((_pauseTime - _pausedTimeSpan) - _initTime)*_secondsPerCounter);
+		}
+
+		return (float)(((_currentTime - _pausedTimeSpan) - _initTime)*_secondsPerCounter);
 	}
 
-	__int64 currentTime;
-	QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
-	_currentTime = currentTime;
-
-	_deltaTime = (_currentTime - _previousTime)*_secondsPerCounter;
-
-	_previousTime = _currentTime;
-
-	if (_deltaTime < 0.0)
+	float ETimer::GetDeltaTime()const
 	{
-		_deltaTime = 0.0;
+		return (float)_deltaTime;
 	}
-}
 
-void ETimer::Resume()
-{
-	__int64 currentTime;
-	QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
-	
-	if (_isPaused)
-	{
-		_pausedTimeSpan += (currentTime - _pauseTime);
 
-		_previousTime = currentTime;
-		_pauseTime = 0;
-		_isPaused = false;
-	}
-}
-
-void ETimer::Pause()
-{
-	if (!_isPaused)
+	void ETimer::Init()
 	{
 		__int64 currentTime;
 		QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
 
-		_pauseTime = currentTime;
-		_isPaused = true;
+		_initTime = currentTime;
+		_previousTime = currentTime;
+		_pauseTime = 0;
+		_isPaused = false;
+	}
+
+
+	void ETimer::Tick()
+	{
+		if (_isPaused)
+		{
+			_deltaTime = 0.0;
+			return;
+		}
+
+		__int64 currentTime;
+		QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
+		_currentTime = currentTime;
+
+		_deltaTime = (_currentTime - _previousTime)*_secondsPerCounter;
+
+		_previousTime = _currentTime;
+
+		if (_deltaTime < 0.0)
+		{
+			_deltaTime = 0.0;
+		}
+	}
+
+	void ETimer::Resume()
+	{
+		__int64 currentTime;
+		QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
+
+		if (_isPaused)
+		{
+			_pausedTimeSpan += (currentTime - _pauseTime);
+
+			_previousTime = currentTime;
+			_pauseTime = 0;
+			_isPaused = false;
+		}
+	}
+
+	void ETimer::Pause()
+	{
+		if (!_isPaused)
+		{
+			__int64 currentTime;
+			QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
+
+			_pauseTime = currentTime;
+			_isPaused = true;
+		}
 	}
 }
--- EEngine/ETimer.h
+++ EEngine/ETimer.h
@@ -1,27 +1,30 @@
 #pragma once
 
-class ETimer
+namespace EEngine
 {
-public:
-	ETimer();
+	class ETimer
+	{
+	public:
+		ETimer();
 
-	void Init();
-	void Tick();
-	void Pause();
-	void Resume();
+		void Init();
+		void Tick();
+		void Pause();
+		void Resume();
 
-	float GetTotalTime() const;
-	float GetDeltaTime() const;
+		float GetTotalTime() const;
+		float GetDeltaTime() const;
 
-private:
-	double _secondsPerCounter;
-	double _deltaTime;
+	private:
+		double _secondsPerCounter;
+		double _deltaTime;
 
-	__int64 _initTime;
-	__int64 _pausedTimeSpan;
-	__int64 _pauseTime;
-	__int64 _previousTime;
-	__int64 _currentTime;
+		__int64 _initTime;
+		__int64 _pausedTimeSpan;
+		__int64 _pauseTime;
+		__int64 _previousTime;
+		__int64 _currentTime;
 
-	bool _isPaused;
-};
+		bool _isPaused;
+	};
+}
